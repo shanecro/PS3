f04d931aabe698af722d3914e49f766da69cc0be
Shane <Shane@WINDOWS-JF2J1EB.fios-router.home>
2017-03-18 13:04:24 -0400

    fixing exception assertion

--- XMLExample/src/main/java/pkgLibrary/BookOpTest.java
+++ XMLExample/src/main/java/pkgLibrary/BookOpTest.java
@@ -40,14 +40,17 @@ public class BookOpTest {
 		
 //		assertEquals(cat.getBook(cat, "bk123"),b );
 		assertEquals(cat.getBooks().size(), 13 );
-	
-		
-//		assertEquals(cat.addBook(cat, b1), BookException.class );
-		assertEquals(cat.getBooks().size(), 12 );
-		
-		
+			
 		
 		
 	}
-
+	@Test(expected = BookException.class)
+	public void testBookException() throws BookException { 
+		Catalog a = null;
+		Catalog cat = a.ReadXMLFile();//not needed, always the ReadXMLFile as cat in the add and getbook methods
+		Book b1 = new Book();
+		b1.setBookId("bk101");
+		cat.addBook(cat, b1);
+		assertEquals(cat.getBooks().size(), 12 );
+	}
 }
